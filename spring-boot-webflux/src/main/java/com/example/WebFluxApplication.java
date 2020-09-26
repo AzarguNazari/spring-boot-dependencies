0a10ba0d254ddf57d3cac523c2acc25661dd0bca
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-09-26 12:42:24 +0200

    refactor

--- spring-boot-webflux/src/main/java/com/example/WebFluxApplication.java
+++ spring-boot-webflux/src/main/java/com/example/WebFluxApplication.java
@@ -1,12 +1,55 @@
 package com.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "/api")
 public class WebFluxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebFluxApplication.class, args);
 	}
+
+	@GetMapping(value = "/hello")
+	public Mono<Hello> hello(){
+		return Mono.just(new Hello("Hello"));
+	}
+
+	@GetMapping(value = "/names")
+	public Flux<Hello> names(){
+		return Flux.just(new Hello("Nabi"),
+						new Hello("Karim"),
+						new Hello("Mahmood"));
+	}
+
+	public Mono<ServerResponse> helloFunctionalWay(ServerRequest request) {
+		return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+				.body(BodyInserters.fromValue("Hello, functional Spring!"));
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> route(WebFluxApplication greetingHandler) {
+		return RouterFunctions
+				.route(RequestPredicates.GET("/api/hello-reactive")
+				.and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), greetingHandler::helloFunctionalWay);
+	}
+}
+
+@Data
+@AllArgsConstructor
+class Hello{
+	private String message;
 }
--- spring-boot-webflux/src/test/java/com/example/HelloTests.java
+++ spring-boot-webflux/src/test/java/com/example/HelloTests.java
@@ -1,21 +1,33 @@
 package com.example;
 
-import com.example.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@WebFluxTest(controllers = HelloController.class)
+@WebFluxTest(controllers = WebFluxApplication.class)
 public class HelloTests {
+
     @Autowired
     WebTestClient client;
 
     @Test
     public void getHello() {
-        client.get().uri("/hello").exchange().expectStatus().isOk();
+        client.get().uri("/api/hello")
+                    .exchange().expectStatus().isOk()
+                    .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                    .expectBody().jsonPath("$.message").isEqualTo("Hello");
+    }
+
+    @Test
+    public void threeNamesExists() {
+        client.get().uri("/api/names")
+                .exchange().expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody().jsonPath("$[1].message").isEqualTo("Karim");
     }
 }
